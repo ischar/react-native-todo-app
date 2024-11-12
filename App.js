4fd78afc92f5f1bb7ade9c2095185b089145e61c
Jaeho Cha <ischar@naver.com>
2024-11-12 20:24:06 +0900

    refactor(app): update app through component seperation
    - 컴포넌트 분리에 따른 app 수정

--- App.js
+++ App.js
@@ -1,32 +1,64 @@
-import {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
-import styles from './styles';
+import { useState } from "react";
+import { Text, View, FlatList, Button } from "react-native";
+import GoalInput from "./components/GoalInput";
+import GoalItem from "./components/GoalItem";
+import styles from "./styles";
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState('');
-  
-  function goalInputHandler(enteredText) {
-    setEnteredGoalText(enteredText);
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function addGoalHandler(enteredGoalText) {
+    setCourseGoals((currentCourseGoals) => [
+      ...courseGoals,
+      { id: Math.random().toString(), text: enteredGoalText },
+    ]);
+    endAddGoalHandler();
   }
 
-  function addGoalHandler() {
-    console.log(enteredGoalText);
+  function startAddGoalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function endAddGoalHandler() {
+    setModalIsVisible(false);
+  }
+
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
   }
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="할 일을 작성해주세요!"
-          onChangeText={goalInputHandler}
-        />
-        <Button title="Add" onPress={addGoalHandler} />
-      </View>
+      <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} onCancel={endAddGoalHandler} />
       <View style={styles.goalsContainer}>
-        <Text>TODO</Text>
+        <View style={styles.headerContainer}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>TODO</Text>
+          </View>
+          <View style={styles.mainButton}>
+            <Button title="Add" color="#757575" onPress={startAddGoalHandler} />
+          </View>
+        </View>
+
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        ></FlatList>
       </View>
     </View>
   );
 }
-
